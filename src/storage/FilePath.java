dc9d78efbb6f56673a4691809846f6d071075873
hang93 <chenghang@live.com.sg>
2016-02-24 21:43:32 +0800

    Added function for changing default save location

---
+++ src/storage/FilePath.java
@@ -0,0 +1,63 @@
+package storage;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FilePath {
+
+        private static final String SAVE_DATA_FILE = "saveInfo.txt";
+        private static final String DATA_FILE_NAME = "data.csv";
+    
+        
+        /**
+         * Changes the default directory location to store the data file to the provided path.
+         * Path provided MUST be valid.
+         * <p>
+         * @param saveDir Location of new directory to contain data file for saved tasks
+         */
+        public static void changeDirectory(Path saveDir) {
+            //boolean isValidDir = checkValidDir();
+            
+            FileWriter fileWriter = null;
+            try {
+                fileWriter = new FileWriter(SAVE_DATA_FILE , false);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.print(saveDir.toString());
+            printWriter.close();
+        }
+        /**
+         * Returns the path of the file containing saved tasks. If no path is defined, 
+         * sets the default save location to the working directory and returns the path
+         * of the file to be created in the working directory.
+         * <p>
+         * @return String of path of the file containing saved tasks
+         */
+        public static String getPath() {
+            Path saveDir = Paths.get(".");
+            try {
+                BufferedReader fileReader = new BufferedReader(new FileReader(SAVE_DATA_FILE));
+                saveDir = Paths.get(fileReader.readLine());
+                fileReader.close();
+            } catch (IOException e) {
+                changeDirectory(saveDir);
+                Path filePath = Paths.get(".", DATA_FILE_NAME);
+                return filePath.toString();
+            }
+            Path filePath = Paths.get(saveDir.toString(), DATA_FILE_NAME);
+            return filePath.toString();
+        }
+        
+        public static void copyFile(String copyDirectory) {
+            
+        }
+        
+}
