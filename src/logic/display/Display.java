9a6b79d69ff22374423c42c7f1731b26b1bbbcc1
Ruibin <ruibinch@gmail.com>
2016-02-25 16:00:57 +0800

    Changed Search class to be an inheritance of Display class. Implemented unit testing for display and search.

--- src/logic/display/Display.java
+++ src/logic/display/Display.java
@@ -1,4 +1,5 @@
-package logic;
+package logic.display;
+import logic.TaskObject;
 
 import java.util.ArrayList;
 
@@ -20,12 +21,26 @@ public class Display {
 	
 	private ArrayList<TaskObject> taskList;
 	private ArrayList<String> output = new ArrayList<String>();
+	
+	public Display() {
+		
+	}
 
 	public Display(ArrayList<TaskObject> taskList) {
 		this.taskList = taskList;
 	}
 	
-	ArrayList<String> run() {
+	// For search function
+	public ArrayList<String> runSpecificList(ArrayList<TaskObject> newTaskList) {
+		this.taskList = newTaskList;
+		return this.display();
+	}
+	
+	public ArrayList<String> run() {
+		return this.display();
+	}
+	
+	private ArrayList<String> display() {
 		if (taskList.isEmpty()) {
 			outputEmptyMessage();
 		} else {
@@ -45,16 +60,16 @@ public class Display {
 				 */
 				switch (taskCategory) {
 					case "deadline":
-						String taskStartDateInOutputFormat = parseDate(taskStartDate);
-						outputDeadlineTask(i, taskTitle, taskStartDateInOutputFormat, taskStartTime, taskStatus);
+						String taskEndDateInOutputFormat = parseDate(taskEndDate);
+						outputDeadlineTask(i+1, taskTitle, taskEndDateInOutputFormat, taskEndTime, taskStatus);
 						break;
 					case "event":
-						taskStartDateInOutputFormat = parseDate(taskStartDate);
-						String taskEndDateInOutputFormat = parseDate(taskEndDate);
-						outputEventTask(i, taskTitle, taskStartDateInOutputFormat, taskEndDateInOutputFormat, taskStartTime, taskEndTime, taskStatus);
+						String taskStartDateInOutputFormat = parseDate(taskStartDate);
+						taskEndDateInOutputFormat = parseDate(taskEndDate);
+						outputEventTask(i+1, taskTitle, taskStartDateInOutputFormat, taskEndDateInOutputFormat, taskStartTime, taskEndTime, taskStatus);
 						break;
 					case "floating":
-						outputFloatingTask(i, taskTitle, taskStatus);
+						outputFloatingTask(i+1, taskTitle, taskStatus);
 						break;
 					default:
 						break;
@@ -80,8 +95,8 @@ public class Display {
 		return day + "/" + month + "/" + year;
 	}
 	
-	private void outputDeadlineTask(int num, String taskTitle, String taskStartDate, int taskStartTime, String taskStatus) {
-		output.add(String.format(DISPLAY_RESULT_DEADLINE, num, taskTitle, taskStartDate, taskStartTime, taskStatus));
+	private void outputDeadlineTask(int num, String taskTitle, String taskEndDate, int taskEndTime, String taskStatus) {
+		output.add(String.format(DISPLAY_RESULT_DEADLINE, num, taskTitle, taskEndDate, taskEndTime, taskStatus));
 	}
 	
 	private void outputEventTask(int num, String taskTitle, String taskStartDate, String taskEndDate, 
