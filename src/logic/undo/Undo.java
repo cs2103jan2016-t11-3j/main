82b46211c3b83f73a908ab6a9acb8ec6c881e104
Ruibin <ruibinch@gmail.com>
2016-03-05 04:01:48 +0800

    Edited Undo function to no longer be a child of Logic - it instead creates a secondary Logic class inside Undo to process the undo operation.

--- src/logic/undo/Undo.java
+++ src/logic/undo/Undo.java
@@ -2,24 +2,46 @@ package logic.undo;
 import logic.*;
 
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.Stack;
 
-public class Undo extends Logic {
+public class Undo {
+
+	public static final int INDEX_ADD = 1;
+	public static final int INDEX_EDIT = 3;
+	public static final int INDEX_DELETE = 4;
+	private static final String MESSAGE_UNDO = "%1$s undone.";
+	private static final String MESSAGE_UNDO_ERROR = "Nothing to undo!";
 	
-	private static final String MESSAGE_UNDO = "%1$s undone";
-	
+	private ArrayList<TaskObject> taskList;
 	private Stack<CommandObject> undoList;
 	private ArrayList<String> output = new ArrayList<String>();
 
-	public Undo(Stack<CommandObject> undoList) {
+	public Undo(ArrayList<TaskObject> taskList, Stack<CommandObject> undoList) {
+		this.taskList = taskList;
 		this.undoList = undoList;
 	}
 	
+	public ArrayList<TaskObject> getTaskList() {
+		return taskList;
+	}
+	
+	public Stack<CommandObject> getUndoList() {
+		return undoList;
+	}
+	
 	public ArrayList<String> run() {
-		CommandObject commandObj = undoList.pop();
+		try {
+			CommandObject commandObj = undoList.pop();
 		
-		super.parseCommandObject(commandObj, true);
-		output.add(String.format(MESSAGE_UNDO, getUndoneCommandType(commandObj)));
+			Logic secondaryLogic = new Logic(taskList, undoList);
+			secondaryLogic.parseCommandObject(commandObj, true);
+			// The primary logic class will then call getters to get the updated lists
+			
+			output.add(String.format(MESSAGE_UNDO, getUndoneCommandType(commandObj)));
+		} catch (EmptyStackException e) {
+			System.out.println(MESSAGE_UNDO_ERROR);
+		}
 		
 		return output;
 	}
@@ -27,11 +49,11 @@ public class Undo extends Logic {
 	private String getUndoneCommandType(CommandObject commandObj) {
 		switch (commandObj.getCommandType()) {
 			case INDEX_ADD :
-				return "delete";
+				return "Delete";
 			case INDEX_DELETE :
-				return "add";
+				return "Add";
 			case INDEX_EDIT :
-				return "edit";
+				return "Edit";
 			default :
 				return "";
 		}
