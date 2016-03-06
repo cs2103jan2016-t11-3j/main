a93e66c8060cfefe1e53ba1498003630afc2b8eb
Ruibin <ruibinch@gmail.com>
2016-03-07 02:22:50 +0800

    Fixed a bug with Undo/Redo.

--- src/logic/delete/Delete.java
+++ src/logic/delete/Delete.java
@@ -151,7 +151,7 @@ public class Delete {
 
 	private void obtainTaskId() {
 		int index = commandObj.getIndex();
-		if (index > 0 && index <= taskList.size()) { 
+		if (index > 0 && index <= lastOutputTaskList.size()) { 
 			index--;
 			taskIdToDelete = lastOutputTaskList.get(index).getTaskId();
 		}
