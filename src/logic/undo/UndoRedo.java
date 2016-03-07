6f87fa02b6aad181f6c8f1a03f9cbcf3ca0f6a18
Ruibin <ruibinch@gmail.com>
2016-03-07 13:41:37 +0800

    Cleaned up code - removed redundant imports of Stack.

--- src/logic/undo/UndoRedo.java
+++ src/logic/undo/UndoRedo.java
@@ -2,16 +2,12 @@ package logic.undo;
 
 import java.util.ArrayList;
 import java.util.Deque;
-import java.util.Stack;
 
 import common.CommandObject;
 import common.TaskObject;
 
 public class UndoRedo {
 
-	private static final int INDEX_ADD = 1;
-	private static final int INDEX_EDIT = 3;
-	private static final int INDEX_DELETE = 4;
 	private static final int INDEX_UNDO = 5;
 	private static final int INDEX_REDO = 6;
 	
