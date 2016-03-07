9db7d2af0eacae043107a240ad7440ae50f5ada8
hang93 <chenghang@live.com.sg>
2016-03-07 15:15:54 +0800

    fix code

--- src/test/LogicDriver.java
+++ src/test/LogicDriver.java
@@ -3,16 +3,20 @@ package test;
 import java.io.IOException;
 import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import logic.Logic;
 
 public class LogicDriver {
     
     private static ArrayList<String> display = new ArrayList<String>();
     private static Logic logic = new Logic();
+    private static Scanner scanner = new Scanner(System.in);
     
     public static void main(String[] args) throws NoSuchFileException, IOException {
             
-        for (String command : args) {
+        while( true ) {
+            c
             display = execute(command);
             print(display);
         }
