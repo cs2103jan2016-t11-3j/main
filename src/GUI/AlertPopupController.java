35822c59395e5e079098a5d6c1805cb15791fa92
Pan Seow Hwee <a0130622@u.nus.edu>
2016-03-24 02:29:04 +0800

    minor edit to alert popup

--- src/GUI/AlertPopupController.java
+++ src/GUI/AlertPopupController.java
@@ -9,15 +9,21 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
 
 public class AlertPopupController implements Initializable {
     
 	@FXML
     private ListView<String> alertTasks;
+    @FXML
+    private VBox alertPane;
     
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		populateList();
+		alertPane.requestFocus();
 	}
 
 	private void populateList() {
@@ -25,6 +31,13 @@ public class AlertPopupController implements Initializable {
 		ObservableList<String> items =FXCollections.observableArrayList(output);
 		
 		alertTasks.setItems(items);
+	}
+	@FXML
+	public void handleEnterPressed(KeyEvent event) {
+		if (event.getCode() == KeyCode.ENTER) {
+			alertPane.setVisible(false);
+			System.out.println("hidden");
+		}
 	}
 
 }
