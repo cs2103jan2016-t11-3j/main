4e14da2f2f9a132acefc1e5bffa7c31c903c74a6
Sly <sylvester.csw@gmail.com>
2016-03-23 22:52:52 +0800

    Fixed parser setting category. No more recurring category.

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -261,8 +261,6 @@ public class Parser {
 	public void setCategory() {
 		if (isFloating()) {
 			TO.setCategory("floating");
-		} else if (isRecurring()) {
-			TO.setCategory("recurring");
 		} else if (isDeadline()) {
 			TO.setCategory("deadline");
 			TO.setEndTime(TO.getStartTime());
@@ -283,14 +281,6 @@ public class Parser {
 	
 	public boolean isDeadline() {
 		if (TO.getEndDateTime().equals(LocalDateTime.MAX)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public boolean isRecurring() {
-		if (TO.getIsRecurring()) {
 			return true;
 		} else {
 			return false;
