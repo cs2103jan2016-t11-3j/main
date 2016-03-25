cccab1c06d924a6a66ae2ef84ef9e0c5ec3c9c8d
Ruibin <ruibinch@gmail.com>
2016-03-26 03:06:19 +0800

    Added generic Pair class to facilitate undo-delete.

---
+++ src/common/Pair.java
@@ -0,0 +1,24 @@
+package common;
+
+public class Pair <S, T>{
+
+	private S first;
+	private T second;
+	
+	public Pair(S first, T second) {
+		this.first = first;
+		this.second = second;
+	}
+	
+	public Pair() {
+		
+	}
+	
+	public S getFirst() {
+		return first;
+	}
+	
+	public T getSecond() {
+		return second;
+	}
+}
