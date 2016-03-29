8ed858ac91ba551b93613d5ca6e8de8cb38271f3
Ruibin <ruibinch@gmail.com>
2016-03-29 12:19:03 +0800

    Fixed compilation errors to test export feature.

--- src/test/storage/SaveTest.java
+++ src/test/storage/SaveTest.java
@@ -1,4 +1,4 @@
-package test.storage;
+package Test.storage;
 
 import static org.junit.Assert.*;
 
--- src/test/storage/StorageTest.java
+++ src/test/storage/StorageTest.java
@@ -1,4 +1,4 @@
-package test.storage;
+package Test.storage;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
