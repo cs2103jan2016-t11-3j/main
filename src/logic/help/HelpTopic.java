2d795f46673758f4995cb03c717150c730a28079
hang93 <chenghang@live.com.sg>
2016-04-02 17:20:35 +0800

    Quick fix for loading help in runnable jar

--- src/logic/help/HelpTopic.java
+++ src/logic/help/HelpTopic.java
@@ -2,9 +2,9 @@ package logic.help;
 
 import java.util.ArrayList;
 
-import java.io.File;
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class HelpTopic {
 
@@ -25,22 +25,20 @@ public class HelpTopic {
 		String num;
 		number = _number;
 		num = Integer.toString(number);
-		File topicFile = constructFile(num);
+		String topicFile = constructFile(num);
 		readFile(topicFile);
 	}
 	
-	public File constructFile(String num) {
-		File fakeFile = new File("");
-		String path = fakeFile.getAbsolutePath(); // To obtain working directory
-		File file = new File(path + FILE_PATH + num + ".txt");
-		return file;
+	public String constructFile(String num) {
+		String helpFilePath = num + ".txt";
+		return helpFilePath;
 	}
 	// Helper function for constructor
-	public void readFile(File topicFile) {
-		String text = "";
-		try {
-			FileReader fileReader = new FileReader(topicFile);
-			BufferedReader reader = new BufferedReader(fileReader);
+	public void readFile(String topicFile) {
+	    InputStream is = this.getClass().getResourceAsStream(topicFile);
+	    String text = "";
+		try { 
+		    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 			name = reader.readLine();
 			text = reader.readLine();
 			while (text != null) {
